86c22627fac5cf8d11bbf06ea17897c69725626a
Le Chi Toan <lechitoan2571999@gmail.com>
2021-04-16 18:23:52 +0700

    Update to Day5 React Developer RoadMaps

--- react-slack-clone/src/component/SidePanel/SidePanel.js
+++ react-slack-clone/src/component/SidePanel/SidePanel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
 import UserPanel from './UserPanel';
+import Channels from './Channels';
 
 class SidePanel extends React.Component {
     render () {
@@ -8,7 +9,7 @@ class SidePanel extends React.Component {
         return (
             <Menu size = "large" inverted fixed ="left" vertical style={{background: '#4c3c4c', fontsize: '1.2em'}}>
                 <UserPanel currentUser={currentUser} />    
-
+                <Channels currentUser={currentUser}/>
             </Menu>
         )
     }
