1a7e88079867995e43bc00287635ac0a243bdbde
Le Chi Toan <lechitoan2571999@gmail.com>
2021-04-20 09:16:28 +0700

    Add Create Messages Collection, Add First Message to Database

--- react-slack-clone/src/component/Messages/MessagesForm.js
+++ react-slack-clone/src/component/Messages/MessagesForm.js
@@ -1,13 +1,70 @@
+import firebase from '../../firebase'
 import React from 'react';
 import { Button, Input, Segment } from 'semantic-ui-react';
 
 class MessagesForm extends React.Component{
+    state = {
+        message: '',
+        channel: this.props.currentChannel,
+        user: this.props.currentUser,
+        loading: false,
+        errors: []
+    }
+
+    handleChange = event => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    createMessage = () => {
+        const message = {
+            timestamp: firebase.database.ServerValue.TIMESTAMP,
+            user : {
+                id: this.state.user.uid,
+                name: this.state.user.displayName,
+                avatar: this.state.user.photoURL
+            },
+            content: this.state.message
+        };
+        console.log("Message in createMessage is: " + message)
+        return message;
+    }
+
+    sendMessage = () => { 
+        const { messagesRef } = this.props;
+        const { message, channel } = this.state;
+        console.log("message is: " + message)
+        if(message){
+            this.setState({loading: true})
+            messagesRef.child(channel.id)
+            .push()
+            .set(this.createMessage())
+            .then(() => {
+                this.setState({loading: false, message: '', errors: []})
+            })
+            .catch(err => {
+                console.error(err);
+                this.setState({
+                    loading: false,
+                    errors: this.state.errors.concat(err)
+                })
+            })
+        }else{
+            this.setState({
+                errors: this.state.errors.concat({message: 'Add a messsage'})
+            })
+        }
+    }
+
     render(){
+        const { errors } = this.state;
         return(
             <Segment className="messsage__form">
-                <Input fluid name="message" style={{marginBottom: '0.7em'}} label={<Button icon={"add"}/>} labelPosition="left" placeholder="Write your message" />
+                <Input fluid name="message" onChange={this.handleChange} style={{marginBottom: '0.7em'}} label={<Button icon={"add"}/>} labelPosition="left" 
+                className={
+                    errors.some(error => error.message.includes('message')) ? 'error' : ''
+                } placeholder="Write your message" />
                 <Button.Group icon widths="2"  >
-                    <Button color="orange" content="Add Reply" labelPosition="left" icon="edit"/>
+                    <Button color="orange" content="Add Reply" labelPosition="left" icon="edit" onClick={this.sendMessage}/>
                     <Button color="teal" content="Upload Media" labelPosition="right" icon="cloud upload"/>
                 </Button.Group>
             </Segment>
